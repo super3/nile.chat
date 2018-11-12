ed9706ac4206f953d262a87b36e964bf05bdee8e
Monty Anderson <work@montyanderson.net>
2018-11-12 13:46:18 +0000

    .gitignore + auto scroll + atomic txs

--- src/components/Channel.vue
+++ src/components/Channel.vue
@@ -20,7 +20,7 @@
             </div>
         </div>
         <!-- Chat messages -->
-        <div class="px-6 py-4 flex-1 overflow-y-scroll">
+        <div class="px-6 py-4 flex-1 overflow-y-scroll" ref="chat">
             <div v-for="message in channel.messages" class="flex items-start mb-4 text-sm">
                 <img v-bind:src="message.image" class="w-10 h-10 rounded mr-3">
 
@@ -33,14 +33,7 @@
                 </div>
             </div>
         </div>
-        <div class="pb-6 px-4 flex-none">
-            <div class="flex rounded-lg border-2 border-grey overflow-hidden">
-                <span class="text-3xl text-grey border-r-2 border-grey p-2">
-                    <svg class="fill-current h-6 w-6 block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M16 10c0 .553-.048 1-.601 1H11v4.399c0 .552-.447.601-1 .601-.553 0-1-.049-1-.601V11H4.601C4.049 11 4 10.553 4 10c0-.553.049-1 .601-1H9V4.601C9 4.048 9.447 4 10 4c.553 0 1 .048 1 .601V9h4.399c.553 0 .601.447.601 1z"/></svg>
-                  </span>
-                <input v-bind:placeholder="'Message #' + channel.name" v-model="message" v-on:keyup.13="handleMessage" type="text" class="w-full px-4" />
-            </div>
-        </div>
+
     </div>
 </template>
 
@@ -57,6 +50,9 @@ module.exports = {
 			this.$emit('message', this.message);
 			this.message = "";
 		}
+	},
+	updated() {
+		this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
 	}
 };
 </script>
