2f358e839fb71657467b0cd97d173c64cebc2289
Monty Anderson <work@montyanderson.net>
2019-01-26 16:12:31 +0000

    explicitly set response type of sitemap as text/xml

--- routers/static.js
+++ routers/static.js
@@ -37,6 +37,8 @@ router.get('/sitemap.xml', async ctx => {
 		}
 	}
 
+	ctx.type = 'text/xml';
+
 	ctx.body = `<?xml version="1.0" encoding="utf-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
